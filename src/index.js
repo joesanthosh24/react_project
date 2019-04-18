1e047c93ad8a328fa38b7732c5cd1fa44150d663
joesanthosh24 <42412473+joesanthosh24@users.noreply.github.com>
2019-04-18 09:37:00 -0400

    Library
    created library component and made some changes to other files

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavBar from "./components/common/navbar";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./styles.css";
 import Books from "./components/books";
+import Movies from "./components/movies";
+import Library from "./components/library";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <Books />
+      <Switch>
+        <Route path="/books" component={Books} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/library" component={Library} />
+        <Redirect from="/" to="/books" />
+      </Switch>
     </React.Fragment>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
