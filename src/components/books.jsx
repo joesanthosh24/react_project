d4153f9d0cd80b47850dbc40f56c684383f93d8a
joesanthosh24 <42412473+joesanthosh24@users.noreply.github.com>
2019-04-17 23:06:45 -0400

    books
    created booksInfo.js file which contains info on books. Edited books.jsx table

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import Pagination from "./common/pagination";
+import { getBooks } from "../services/booksInfo";
 
 class Books extends Component {
   state = {
-    books: []
+    books: getBooks(),
+    pageSize: 3
   };
 
+  handlePageChange() {}
+
   render() {
-    const { books } = this.state;
+    const { books, pageSize } = this.state;
 
     return (
       <div className="container">
-        <table>
+        <table className="table">
           <thead>
             <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Author</th>
+              <th scope="column">Title</th>
+              <th scope="column">Genre</th>
+              <th scope="column">Author</th>
             </tr>
           </thead>
+          <tbody>
+            {books.map(book => (
+              <tr key={book.id}>
+                <td scope="row">{book.title}</td>
+                <td scope="row">{book.genre}</td>
+                <td scope="row">{book.author}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        <Pagination numItems={books} />
+        <Pagination
+          numItems={books.length}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </div>
     );
   }
---
+++ src/services/booksInfo.js
@@ -0,0 +1,60 @@
+const books = [
+  {
+    id: 1,
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    genre: "Fantasy"
+  },
+  {
+    id: 2,
+    title: "The Wizard of Oz",
+    author: "L. Frank Baum",
+    genre: "Fantasy"
+  },
+  {
+    id: 3,
+    title: "Tokyo Ghoul Vol. 5",
+    author: "Sui Ishida",
+    genre: "Manga"
+  },
+  {
+    id: 4,
+    title: "Frankenstein",
+    author: "Mary Shelley",
+    genre: "Fiction"
+  },
+  {
+    id: 5,
+    title: "Mamba Mentality - How I approached the Game",
+    author: "Kobe Bryant",
+    genre: "Non-Fiction"
+  },
+  {
+    id: 6,
+    title: "Design Patterns: Elements of Re-Usable Object Oriented Software",
+    author: "Erich Gamma",
+    genre: "Computer Science"
+  },
+  {
+    id: 7,
+    title: "Oliver Twist",
+    author: "Charles Dickens",
+    genre: "Fiction"
+  },
+  {
+    id: 8,
+    title: "Bleach Volume 1",
+    author: "Tite Kubo",
+    genre: "Manga"
+  },
+  {
+    id: 9,
+    title: "Genki: An integrated Course in Japanese",
+    author: "Eri Banno",
+    genre: "Information"
+  }
+];
+
+export function getBooks() {
+  return books;
+}
