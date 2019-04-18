9dc80be0e30bdaed50ab428f18d6338f0a926e7b
joesanthosh24 <42412473+joesanthosh24@users.noreply.github.com>
2019-04-18 08:26:00 -0400

    Movies Component and Changes
    refactor: books.jsx - changed

---
+++ src/services/moviesInfo.js
@@ -0,0 +1,51 @@
+const movies = [
+  {
+    _id: 1,
+    title: "Terminator",
+    genre: "Action"
+  },
+  {
+    _id: 2,
+    title: "Die Hard",
+    genre: "Action"
+  },
+  {
+    _id: 3,
+    title: "Get Out",
+    genre: "Thriller"
+  },
+  {
+    _id: 4,
+    title: "Trip to Italy",
+    genre: "Comedy"
+  },
+  {
+    _id: 5,
+    title: "Airplane",
+    genre: "Comedy"
+  },
+  {
+    _id: 6,
+    title: "Wedding Crashers",
+    genre: "Comedy"
+  },
+  {
+    _id: 7,
+    title: "Gone Girl",
+    genre: "Thriller"
+  },
+  {
+    _id: 8,
+    title: "The Sixth Sense",
+    genre: "Thriller"
+  },
+  {
+    _id: 9,
+    title: "The Avengers",
+    genre: "Action"
+  }
+];
+
+export function getMovies() {
+  return movies;
+}
