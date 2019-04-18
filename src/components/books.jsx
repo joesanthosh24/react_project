94ee2a8a250cc37bdc9c43349bd0c2092b8762fa
joesanthosh24 <42412473+joesanthosh24@users.noreply.github.com>
2019-04-17 20:05:08 -0400

    Books Component
    Added a Books Component to the react project

---
+++ src/components/books.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const Books = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
+export default Books;
--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import NavBar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
+import Books from "./components/books";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
+      <Books />
     </React.Fragment>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
