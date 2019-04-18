8885151dd4b1646fc3bb5ee8932649dd5ad7a321
joesanthosh24 <42412473+joesanthosh24@users.noreply.github.com>
2019-04-17 20:23:13 -0400

    changes and components
    Created a new component: Pagination and made changes to: index.js, styles.css, books.jsx. Also moved navbar to common folder

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
+import Pagination from "./common/pagination";
 
-const Books = () => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Genre</th>
-          <th>Author</th>
-        </tr>
-      </thead>
-    </table>
-  );
-};
+class Books extends Component {
+  state = {
+    books: []
+  };
+
+  render() {
+    const { books } = this.state;
+
+    return (
+      <div className="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+        </table>
+        <Pagination numItems={books} />
+      </div>
+    );
+  }
+}
 
 export default Books;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NavBar from "./components/navbar";
+import NavBar from "./components/common/navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
